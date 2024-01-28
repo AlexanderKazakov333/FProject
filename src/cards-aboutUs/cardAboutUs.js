8729f0003f27d8b1fc0388477095cf7eee185707
elgiz0210 <dastanadilbekov2019@gmail.com>
2024-01-28 13:47:20 +0600

    first commit

---
+++ src/cards-aboutUs/cardAboutUs.js
@@ -0,0 +1,7 @@
+
+const cardAboutUs = [
+    { id: 1, lat: 51.505, lng: -0.09, title: 'Marker 1' },
+    { id: 2, lat: 51.51, lng: -0.1, title: 'Marker 2' },
+ ]
+
+export default cardAboutUs
--- src/components/AboutUsPage/AboutUsPage.jsx
+++ src/components/AboutUsPage/AboutUsPage.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import logo from '/Users/ADMIN/Desktop/FProject/src/acces/logo.svg'
+import { useNavigate } from 'react-router-dom'
+import '../AboutUsPage/AboutUsPage.css'
+
+
+
+
+
 
 const AboutUsPage = () => {
+
+  const navigate = useNavigate()
+
+  const page2 = () => {
+  navigate('/')
+}
   return (
-    <div>AboutUsPage</div>
+    <div>
+      <div className='Header'>
+        <div>
+          <img className='logo' src={logo} alt="" />
+        </div>
+
+        <div className='menu'>
+          <ul className='lishki'>
+            <li 
+            onClick={page2}
+            className='li1'>Home
+            </li>
+
+            <li className='li'>About Us</li>
+          </ul>
+          <button className='btn1'>+ добавить обьявление</button>
+        </div>
+      </div>
+      <div className='main'>
+        <h1 className='title'>Property Featured</h1>
+        <span className='text'>Real estate is a crowded field. But with the number of buyers purchasing homes <br />through real estate agents and brokers growing there’s plenty of </span>
+      </div>
+    </div>
   )
 }
 
