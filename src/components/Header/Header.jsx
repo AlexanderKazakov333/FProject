c4fa883c8c38b0503dabed640be1aec3c90218ac
elgiz0210 <dastanadilbekov2019@gmail.com>
2024-01-29 10:36:18 +0600

    Merge changes from elgiz into master

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import logo from '../../assets/logo.svg' 
+import { useNavigate } from 'react-router-dom'
+
 
 const Header = () => {
 
- 
+  const navigate = useNavigate() 
+  const page2 = () => { 
+  navigate('/') 
+} 
+  const pageAboutUs = () => {
+    navigate('/aboutUs')
+  }
+
+
+
   return (
     <div className='Header'> 
-        header
+    <div> 
+      <img className='logo' src={logo} alt="" />   
+    </div> 
+
+    <div className='menu'> 
+      <ul className='lishki'> 
+        <li  
+        onClick={page2} 
+        className='li1'>Home 
+        </li> 
+
+        <li 
+        onClick={pageAboutUs}
+        className='li'>About Us
+        </li> 
+      </ul> 
+      <button className='btn1'>+ добавить обьявление</button> 
+    </div> 
   </div> 
   )
 }
--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,6 +1,4 @@
 import React from 'react' 
-import logo from '../../assets/logo.svg' 
-import { useNavigate } from 'react-router-dom' 
 import {aboutUs} from '../../components/AboutUsCard/CardAboutUs' 
 import {aboutUs2} from '../../components/AboutUsCard/CardAboutUs' 
 import img from '../../assets/AboutUsFoto.svg' 
@@ -8,36 +6,15 @@ import '../../pages/AboutUsPage/AboutUsPage.css'
  
  
 const AboutUsPage = () => { 
- 
-  const navigate = useNavigate() 
- 
-  const page2 = () => { 
-  navigate('/') 
-} 
+
+
   return ( 
     <div> 
-      <div className='Header'> 
-        <div> 
-          <img className='logo' src={logo} alt="" />   
-        </div> 
- 
-        <div className='menu'> 
-          <ul className='lishki'> 
-            <li  
-            onClick={page2} 
-            className='li1'>Home 
-            </li> 
- 
-            <li className='li'>About Us</li> 
-          </ul> 
-          <button className='btn1'>+ добавить обьявление</button> 
-        </div> 
-      </div> 
       <div className='main'> 
         <h1 className='title'>Property Featured</h1> 
         <span className='text'>Real estate is a crowded field. But with the number of buyers purchasing homes <br />through real estate agents and brokers growing there’s plenty of </span> 
-        <div className='card-grid'> 
- 
+        <div className='card-grid2'> 
+
         <div> 
         {aboutUs.map((item,idx) => { 
         return( 
